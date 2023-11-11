a68db8534f99b01a2e036cfc553f8f98d3d31134
FikaKarin <mailtolundqvist@gmail.com>
2023-11-11 12:57:38 +0100

    set dist as output directory

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: 'dist', // Explicitly set the output directory
     rollupOptions: {
       external: ['react-router-dom'],
     },
